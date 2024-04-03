9d291a51149b1e0b5e20ec9a315a5e3660ca4665
Evans-Asiedu <evansasiedu352@gmail.com>
2024-04-03 22:35:45 +0000

    Add the user service and registration

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
+import * as userService from "../services/userService";
 
 class RegisterForm extends Form {
   state = {
@@ -18,9 +19,9 @@ class RegisterForm extends Form {
     name: Joi.string().required().label("Name"),
   };
 
-  doSubmit = () => {
+  doSubmit = async () => {
     // call server
-    console.log("submitted");
+    await userService.register(this.state.data);
   };
 
   render() {
